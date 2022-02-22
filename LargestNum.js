d6503176b7f3b0dbf556dfcc6611b82b6f55ebb9
raeez nazar <raeeznazar>
2022-02-23 00:01:34 +0530

    day 1

---
+++ LargestNum.js
@@ -0,0 +1,13 @@
+var num1=600, num2=40;
+
+//if(num1>num2){
+  //  console.log("num1 is greater");
+//}else{
+///   console.log("num2 is greater");
+//}
+
+console.log(num1>num2? "num1 is largest" : "num2 is largest");
+
+
+/// ternary operator
+// condition?true statement : else statemnt
---
+++ NumCheckPositiveOrNegative.js
@@ -0,0 +1,9 @@
+var num = -10;
+
+//if (num>0){ 
+  //  console.log("num is +ve");
+//}else{
+  //  console.log("num is -ve");
+//}
+
+console.log(num>0?"num is positive" : "num is -ve" );
